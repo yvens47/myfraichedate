552459a23273c002d2119daf380e7c31155e13b9
Jean Pierre <yvens47@yahoo.com>
2013-08-13 15:18:05 -0400

    fixed login page

--- includes/user.php
+++ includes/user.php
@@ -52,6 +52,9 @@ class User extends DB {
 		
 		
 }
+function login($username, $password) {
+	
+}
 
 
 }
--- login.php
+++ login.php
@@ -1,4 +1,33 @@
 <?php  require 'header.php';?>
-Login form
+<div class='row'>
+<div class='span8' id='main'>
+	<div class ='social'>
+	<h1>Facebbok</h1>
+	
+	</div>
+	<div class='login'>
+	<div class='wrap'>
+	<h1>Login</h1>
+	<form action="login.php" method="get" class='horizontal'>
+		<div class=''>
+			<label>Username</label>
+			<input class='span3' name='username'/>
+		</div>
+		<div>
+			<label>Password</label>
+			<input class='span3' name='password'/>
+		</div>
+		<div>
+			
+			<input class='btn btn-success' name='login' value='Login' type='submit'/>
+		</div
+	
+	
+	</form>
+	</div>
+</div>
+
+</div>
+</div>
 
 <?php  require 'footer.php';?>
